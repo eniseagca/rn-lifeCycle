81bf65a19a952480b33c7ad95b0808a47770f04e
Enise Ağca <eniseagca@gmail.com>
2022-02-06 20:31:17 +0300

    update

--- App.js
+++ App.js
@@ -1,27 +1,50 @@
 import React, { useState ,useEffect ,useLayoutEffect, Children} from 'react'
-import Child from './Child'
-import {Box,FlatList,HStack,Input, NativeBaseProvider, ScrollView, Text , ActivityIndicator} from 'native-base'
-
+import {Box,FlatList,HStack, NativeBaseProvider, Text } from 'native-base'
+import { ActivityIndicator } from 'react-native'
 
 const App = () => {
 const [value, setValue] = useState([])
+const [status, setStatus]=useState(true)
 
 function getData(){
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => {return response.json()}).then(res=>{setValue(res)})
+  setStatus(false)
 }
 
 
 useEffect(() => {
-  console.log('parent calisti')
-  getData()
-  }
-   , [])
+  setTimeout(()=> {
+    getData()
+  }, 1000)
+  },[])
 
 return(
   <NativeBaseProvider>
   <Box mt="20">
-   <Child value={value} />
+  {status  ? <ActivityIndicator  size="large"/> : 
+               
+
+                <Box> <Box mx="10">
+               <HStack space={'3'} >
+                 <Text fontWeight={'bold'} w="4">Id</Text>
+                 <Text fontWeight={'bold'} w="40" h="10">Name</Text>
+                 <Text fontWeight={'bold'} w="40" h="10">Company Name</Text>
+               </HStack>
+               </Box>
+               
+               <FlatList data={value} keyExtractor={(item)=>item.id} renderItem={({item,id}) =>(
+             
+                <Box mx="10">
+                        <HStack space={'3'} >
+                               <Text w="4">{item.id}</Text>
+                               <Text w="40" h="10"> {item.name}</Text>
+                               <Text w="40" h="10">{item.company.name}</Text>
+                       </HStack>
+                </Box>
+                ) }
+                />
+               </Box>}
   </Box>
   </NativeBaseProvider>
 )
